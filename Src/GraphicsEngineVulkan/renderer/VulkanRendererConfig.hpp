47e84f82f15bd23b7c3f6b0d68791377595aa172
Jonas Heinle <jonasheinle@googlemail.com>
2025-11-13 19:50:00 +0100

    [build-win]

--- Src/GraphicsEngineVulkan/renderer/VulkanRendererConfig.hpp
+++ Src/GraphicsEngineVulkan/renderer/VulkanRendererConfig.hpp
@@ -3,9 +3,9 @@
 #pragma once
 
 #define VulkanRenderer_VERSION_MAJOR "1"
-#define VulkanRenderer_VERSION_MINOR "3"
+#define VulkanRenderer_VERSION_MINOR "4"
 
 #define VULKAN_VERSION_MAJOR "1"
 #define VULKAN_VERSION_MINOR "3"
 
-#define GLSLC_EXE "/home/jones/Documents/1.3.296.0/x86_64/bin/glslc"
+#define GLSLC_EXE "/home/bigjuicyjones/1.4.321.1/aarch64/bin/glslc"
