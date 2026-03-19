5c58ff6d1dcf4590d783d5fcbfbc5e9c55b7893f
Jonas Heinle <jonasheinle@googlemail.com>
2026-03-19 08:01:03 +0100

    [build-arm]

--- Src/GraphicsEngineVulkan/vulkan_base/VulkanDevice.cpp
+++ Src/GraphicsEngineVulkan/vulkan_base/VulkanDevice.cpp
@@ -1,7 +1,5 @@
 module;
 
-#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
-
 #include "renderer/SwapChainDetails.hpp"
 #include <cstdint>
 #include <cstring>
@@ -15,8 +13,6 @@ module;
 #include <set>
 #include <string>
 #include <vulkan/vulkan.hpp>
-
-VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
 
 module kataglyphis.vulkan.device;
 
