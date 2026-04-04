760c2b0696b66bbb46328ae945b0eb0c7a4df179
Jonas Heinle <jonasheinle@googlemail.com>
2026-04-04 23:10:49 +0200

    [build-win][build-arm]

--- cmake/KataglyphisCMakeHelpers.cmake
+++ cmake/KataglyphisCMakeHelpers.cmake
@@ -1,36 +1,23 @@
 include(CMakeParseArguments)
 
-# Collects C++20 module interface files (.ixx) from a directory.
-# Sets two variables in parent scope:
-#   ${out_files_var} - List of absolute paths to module interface files
-#   ${out_base_dir_var} - The canonical absolute base directory (for use in FILE_SET BASE_DIRS)
-#
-# Usage:
-#   kataglyphis_collect_module_interfaces(MY_FILES MY_BASE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/modules")
-#
-function(kataglyphis_collect_module_interfaces out_files_var out_base_dir_var base_dir)
-  # Use file(REAL_PATH) to get a truly canonical absolute path
-  # This is critical for Windows where clang-cl module scanning requires exact path matching
-  file(REAL_PATH "${base_dir}" _real_base_dir)
-  
+function(kataglyphis_collect_module_interfaces out_var base_dir)
+  string(
+    REGEX
+    REPLACE "/+$"
+            ""
+            base_dir
+            "${base_dir}")
   file(
     GLOB_RECURSE _module_interface_relative_files
-    RELATIVE "${_real_base_dir}"
-    "${_real_base_dir}/*.ixx")
+    RELATIVE "${base_dir}"
+    "${base_dir}/*.ixx")
   list(SORT _module_interface_relative_files)
 
-  # Build absolute paths for each file using the same canonical base
-  set(_absolute_files "")
-  foreach(_rel_file IN LISTS _module_interface_relative_files)
-    file(REAL_PATH "${_real_base_dir}/${_rel_file}" _abs_file)
-    list(APPEND _absolute_files "${_abs_file}")
-  endforeach()
+  set(_module_interface_files "${_module_interface_relative_files}")
+  list(TRANSFORM _module_interface_files PREPEND "${base_dir}/")
 
-  set(${out_files_var}
-      "${_absolute_files}"
-      PARENT_SCOPE)
-  set(${out_base_dir_var}
-      "${_real_base_dir}"
+  set(${out_var}
+      "${_module_interface_files}"
       PARENT_SCOPE)
 endfunction()
 
